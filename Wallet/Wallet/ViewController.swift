39725e13729014db32d4a2fcd2783ad920db1579
hanbyeol <whdlgksquf5@naver.com>
2023-03-13 15:14:13 +0900

    프로젝트 파일 생성

---
+++ Wallet/Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by 김한별 on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
