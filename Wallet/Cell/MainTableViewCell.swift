9091cd0c83399400382a50815846a23939ec5ff2
YunHyeonJeong <wjd6408@gmail.com>
2023-03-20 22:12:11 +0900

    [UPDATE] Main 디자인 수정

--- Wallet/Cell/MainTableViewCell.swift
+++ Wallet/Cell/MainTableViewCell.swift
@@ -24,5 +24,14 @@ class MainTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imgView.layer.cornerRadius = imgView.frame.height / 10
+        imgView.layer.borderWidth = 0
+        imgView.layer.borderColor = UIColor.clear.cgColor
+        // 뷰의 경계에 맞춰준다
+        imgView.clipsToBounds = true
+    }
 
 }
