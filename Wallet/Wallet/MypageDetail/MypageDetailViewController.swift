3e14e30015d15e2f208b78e60f2dfff92f0a84c2
subin7709 <tnqls103605@naver.com>
2023-03-21 11:10:52 +0900

    Mypage

---
+++ Wallet/Wallet/MypageDetail/MypageDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MypageDetailViewController.swift
+//  Wallet
+//
+//  Created by 안수빈 on 2023/03/21.
+//
+
+import UIKit
+
+class MypageDetailViewController: UIViewController {
+
+    @IBOutlet weak var ProfileImage: UIImageView!
+    
+    @IBOutlet weak var tfNickName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
