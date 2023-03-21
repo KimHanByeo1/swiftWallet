cd0152e1ed43597a90c775942daebe425ddb808a
kimjn132 <kjhi8309@gmail.com>
2023-03-21 14:18:41 +0900

    mypage추가

---
+++ Wallet/Wallet/MyPages/Cell/ProfileTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileTableViewCell.swift
+//  Wallet
+//
+//  Created by Anna Kim on 2023/03/21.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+//        profileImage.contentMode =
+    }
+
+}
