985cfad6235c48990c6005d2d2fb476d2a354aaa
김한별 <whdlgksquf5@naver.com>
2023-03-23 16:02:07 +0900

    Revert "Ahnsubin"

---
+++ Wallet/Wallet/MypageDetail/Cell/MyPageLikeTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  MyPageLikeTableViewCell.swift
+//  Wallet
+//
+//  Created by Anna Kim on 2023/03/22.
+//
+
+import UIKit
+
+class MyPageLikeTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var pImage: UIImageView!
+    @IBOutlet weak var pTitle: UILabel!
+    @IBOutlet weak var pBrand: UILabel!
+    @IBOutlet weak var pPrice: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+//    override func layoutSubviews() {
+//        pImage.layer.cornerRadius = pImage.frame.height / 8
+//        pImage.layer.borderWidth = 0
+//        pImage.layer.borderColor = UIColor.clear.cgColor
+//        // 뷰의 경계에 맞춰준다
+//        pImage.clipsToBounds = true
+//    }
+
+}
