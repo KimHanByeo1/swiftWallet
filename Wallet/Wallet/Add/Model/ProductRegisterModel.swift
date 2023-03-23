e93b68f2206b0d4f12d88ffcabd84535809fee37
hanbyeol <whdlgksquf5@naver.com>
2023-03-23 15:48:26 +0900

    채팅

--- Wallet/Wallet/Add/Model/ProductRegisterModel.swift
+++ Wallet/Wallet/Add/Model/ProductRegisterModel.swift
@@ -11,7 +11,7 @@ import Firebase
 class ProductRegisterModel {
     
     let db = Firestore.firestore()
-    func insesrtItems(wBrand: String, wMaterial: String, wColor: String, wSize: String, wName: String, wPrice: String, wContent: String, image: String, wTitle: String, wTime: String, wDetailContent: String) -> Bool{
+    func insesrtItems(wBrand: String, wMaterial: String, wColor: String, wSize: String, wName: String, wPrice: String, wContent: String, image: String, wTitle: String, wTime: String, wDetailContent: String, userEmail: String, userNickName: String) -> Bool{
         var status: Bool = true
         
         db.collection("product").addDocument(data: [
@@ -26,7 +26,9 @@ class ProductRegisterModel {
             "pTitle": wTitle,
             "pTime": wTime,
             "pState": "0",
-            "pDetailContent": wDetailContent
+            "pDetailContent": wDetailContent,
+            "userEmail": userEmail,
+            "userNickName": userNickName
         ]){error in
             if error != nil{
                 status = false
