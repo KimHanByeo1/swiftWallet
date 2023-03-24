d482e39c79cf0e069b9f5d249c83038580e85a35
javanyam <elyje5907@gmail.com>
2023-03-24 23:41:14 +0900

    channelViewModel add

---
+++ Wallet/Wallet/Chat/Model/Channel.swift
@@ -0,0 +1,18 @@
+//
+//  Channel.swift
+//  Wallet
+//
+//  Created by 예띤 on 2023/03/24.
+//
+
+struct Channel{
+    var myEmail: String
+    var otherEmail: String
+    var otherName: String
+    
+    init(myEmail: String, otherEmail: String, otherName: String) {
+        self.myEmail = myEmail
+        self.otherEmail = otherEmail
+        self.otherName = otherName
+    }
+}
