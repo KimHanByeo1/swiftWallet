d99e92de6260e2c279068b58dc75b8b8cb41ec45
subin7709 <tnqls103605@naver.com>
2023-03-27 11:33:09 +0900

    수정 중

--- Wallet/Wallet/MyPagePurchase/Model/PurchaseDBModel.swift
+++ Wallet/Wallet/MyPagePurchase/Model/PurchaseDBModel.swift
@@ -9,21 +9,41 @@ import Foundation
 
 struct PurchaseDBModel{
     var documentId: String
-    var userEmail:String // 물건 등록자 이메일
     var puchaseEmail:String // 물건 구매자 이메일
-    var userId:String //물건 등록자 documentId
-    var imageURL:String //물건 이미지
-    var pTitle:String
-    var pPrice:String
+    var imageURL: String
+    var pBrand: String
+    var pColor: String
+    var pContent: String
+    var pDetailContent: String
+    var pMaterial: String
+    var pName: String
+    var pPrice: String
+    var pSize: String
+    var pState: String
+    var pTime: String
+    var pTitle: String
+    var userEmail: String
+    var userId:String
+    var userNickName: String
     
-    init(documentId: String, userEmail: String, puchaseEmail: String, userId: String, imageURL: String, pTitle: String, pPrice: String) {
+    init(documentId: String, puchaseEmail: String, imageURL: String, pBrand: String, pColor: String, pContent: String, pDetailContent: String, pMaterial: String, pName: String, pPrice: String, pSize: String, pState: String, pTime: String, pTitle: String, userEmail: String, userId: String, userNickName: String) {
         self.documentId = documentId
-        self.userEmail = userEmail
         self.puchaseEmail = puchaseEmail
-        self.userId = userId
         self.imageURL = imageURL
-        self.pTitle = pTitle
+        self.pBrand = pBrand
+        self.pColor = pColor
+        self.pContent = pContent
+        self.pDetailContent = pDetailContent
+        self.pMaterial = pMaterial
+        self.pName = pName
         self.pPrice = pPrice
+        self.pSize = pSize
+        self.pState = pState
+        self.pTime = pTime
+        self.pTitle = pTitle
+        self.userEmail = userEmail
+        self.userId = userId
+        self.userNickName = userNickName
     }
     
 }
